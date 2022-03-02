94e1c9c431da87b16121d79b46d4345aa893cc5d
Debabrata909 <72221706+Debabrata909@users.noreply.github.com>
2022-03-02 16:26:54 +0530

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://tutopiacallaz.azurewebsites.net/api/',
-  urlDatbase : 'Mobile_Func_Adv',
-  urlcode : 'code=CLSiAdJbe7iil5hQ9n8aVAOmiFt3KPjk2AnARj3vaY7mjKSsHBxSmg=='
+  baseUrl: 'https://tutopiacallaz.azurewebsites.net/api',
+  urlDatbase : '/Mobile_Func_Adv',
+  urlcode : '?code=CLSiAdJbe7iil5hQ9n8aVAOmiFt3KPjk2AnARj3vaY7mjKSsHBxSmg=='
 };
 export const api = {
   TutopiacallDataBaseUrl : `${environment.baseUrl+environment.urlDatbase+environment.urlcode}`
