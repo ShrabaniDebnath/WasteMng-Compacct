e3b094b3595f4aa60f7ef85d43d053389a550581
Debabrata909 <72221706+Debabrata909@users.noreply.github.com>
2022-03-02 13:23:09 +0530

    update environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,5 +5,5 @@ export const environment = {
   urlcode : 'code=CLSiAdJbe7iil5hQ9n8aVAOmiFt3KPjk2AnARj3vaY7mjKSsHBxSmg=='
 };
 export const api = {
-  TutopiacallDataBaseUrl : `${environment.baseUrl} + ${environment.urlDatbase} + ${environment.urlcode}`
+  TutopiacallDataBaseUrl : `${environment.baseUrl+environment.urlDatbase+environment.urlcode}`
  }
