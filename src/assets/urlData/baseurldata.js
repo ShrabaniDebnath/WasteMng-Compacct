289005be9f22d04ced46b28f7f6df027051a6ee7
Debabrata909 <72221706+Debabrata909@users.noreply.github.com>
2022-03-04 16:47:48 +0530

    update

--- src/assets/urlData/baseurldata.js
+++ src/assets/urlData/baseurldata.js
@@ -1,11 +1,11 @@
 window.config = {
     production: false,
-    baseUrl: 'https://tutopiacallaz.azurewebsites.net/api',
-    urlDatbase : '/Mobile_Func_Adv',
-    urlcode : '?code=CLSiAdJbe7iil5hQ9n8aVAOmiFt3KPjk2AnARj3vaY7mjKSsHBxSmg=='
-  // baseUrl: 'https://hytoneazmain.azurewebsites.net/api',
-  // urlDatbase : '/CommonFunc',
-  // urlcode : '?code=INEvOcX8o/iXaUfI031hRZNMnhaPT0GGI8tTm8Ia4oXEmR/ONbFAoA=='
+    // baseUrl: 'https://tutopiacallaz.azurewebsites.net/api',
+    // urlDatbase : '/Mobile_Func_Adv',
+    // urlcode : '?code=CLSiAdJbe7iil5hQ9n8aVAOmiFt3KPjk2AnARj3vaY7mjKSsHBxSmg=='
+    baseUrl: 'https://hytoneazmain.azurewebsites.net/api',
+    urlDatbase : '/CommonFunc',
+    urlcode : '?code=INEvOcX8o/iXaUfI031hRZNMnhaPT0GGI8tTm8Ia4oXEmR/ONbFAoA=='
   };
   window.api = {
    TutopiacallDataBaseUrl : `${window.config.baseUrl+window.config.urlDatbase+window.config.urlcode}`
